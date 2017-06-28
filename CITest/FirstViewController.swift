3dae7021b6b40e78fc47eab5bb763c71a37e5ac7
Hua Duong Nguyen <hua-duong.nguyen@dvag.com>
2017-06-28 11:57:55 +0200

    added gitignore, added template iOS project

---
+++ CITest/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CITest
+//
+//  Created by Nguyen, Hua Duong on 28.06.17.
+//  Copyright © 2017 Nguyen, Hua Duong. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
